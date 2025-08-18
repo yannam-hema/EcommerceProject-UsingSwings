0bcdbdc905a79c9835d327200066d2909278b14c
yannam-hema <yannamhema30@gmail.com>
2025-08-18 16:07:29 +0530

    Made changes in MenuBuilder by making code more readable

--- SwingProject_Ecommerce/src/com/info/SwingsProject/MenuBuilder.java
+++ SwingProject_Ecommerce/src/com/info/SwingsProject/MenuBuilder.java
@@ -5,80 +5,63 @@ import javax.swing.JMenuItem;
 import javax.swing.JMenu;
 
 public class MenuBuilder {
-	
-	public JMenuBar createMainMenu() {
-		
-		JMenuBar mb = new JMenuBar();
-		
-		      //creating Main menus
-				JMenu electronicsMenu = new JMenu("Electronics");
-				JMenu FurnitureMenu = new JMenu("Furniture");
-				JMenu ClothingMenu = new JMenu("Clothing");
-				JMenu groceriesMenu = new JMenu("grocery");
-				
-				mb.add(electronicsMenu);
-				mb.add(FurnitureMenu);
-				mb.add(ClothingMenu);
-				mb.add(groceriesMenu);
-				
-				//Electronic Menu Sub Menus
-				JMenuItem mobiles = new JMenuItem("Mobiles");
-				JMenuItem laptop = new JMenuItem("Laptop");
-				JMenuItem camera = new JMenuItem("Camera");
-				JMenuItem headPhones = new JMenuItem("Head Phones");
-				JMenuItem Mouse = new JMenuItem("Mouse");
-				
-				//adding Electronics SubMenu Items into Electronics Menu
-				electronicsMenu.add(mobiles);
-				electronicsMenu.add(laptop);
-				electronicsMenu.add(camera);
-				electronicsMenu.add(headPhones);
-				electronicsMenu.add(Mouse);
-				
-				//Furniture Menu Sub Menus
-				JMenuItem sofa = new JMenuItem("Sofa");
-				JMenuItem table = new JMenuItem("Table");
-				JMenuItem chair = new JMenuItem("Chair");
-				JMenuItem bed = new JMenuItem("Bed");
-				JMenuItem couch = new JMenuItem("Couch");
-						
-				//adding Furniture SubMenu Items into Furniture Menu
-				FurnitureMenu.add(sofa);
-				FurnitureMenu.add(table);
-				FurnitureMenu.add(chair);
-				FurnitureMenu.add(bed);
-				FurnitureMenu.add(couch);
-				
-				//Clothing Menu Menu Sub Menus
-				JMenuItem Women = new JMenuItem("Women");
-				JMenuItem Men = new JMenuItem("Men");
-				JMenuItem Kids = new JMenuItem("Kids");
-				JMenuItem Girls = new JMenuItem("Girls");
-				JMenuItem Boys = new JMenuItem("Boys");
-				
-			   //adding Furniture SubMenu Items into Furniture Menu
-				ClothingMenu.add(Women);
-				ClothingMenu.add(Men);
-				ClothingMenu.add(Kids);
-				ClothingMenu.add(Girls);
-				ClothingMenu.add(Boys);
 
-				//Groceries Menu Sub Menus
-				JMenuItem Fruits = new JMenuItem("Fruits");
-				JMenuItem Vegtables = new JMenuItem("Vegtables");
-				JMenuItem DairyProducts  = new JMenuItem("DairyProducts");
-				JMenuItem Rice = new JMenuItem("Rice");
-				JMenuItem Snacks= new JMenuItem("Snacks");
-				
-				//adding Groceries SubMenu Items into Groceries Menu
-				groceriesMenu.add(Fruits);
-				groceriesMenu.add(Vegtables);
-				groceriesMenu.add(DairyProducts);
-				groceriesMenu.add(Rice);
-				groceriesMenu.add(Snacks);
-				
-		
-		return mb;
-	}
+    public JMenuBar createMainMenu() {
+        JMenuBar mb = new JMenuBar();
 
+        mb.add(createElectronicsMenu());
+        mb.add(createFurnitureMenu());
+        mb.add(createClothingMenu());
+        mb.add(createGroceriesMenu());
+
+        return mb;
+    }
+
+    private JMenu createElectronicsMenu() {
+        JMenu electronicsMenu = new JMenu("Electronics");
+
+        electronicsMenu.add(new JMenuItem("Mobiles"));
+        electronicsMenu.add(new JMenuItem("Laptop"));
+        electronicsMenu.add(new JMenuItem("Camera"));
+        electronicsMenu.add(new JMenuItem("Head Phones"));
+        electronicsMenu.add(new JMenuItem("Mouse"));
+
+        return electronicsMenu;
+    }
+
+    private JMenu createFurnitureMenu() {
+        JMenu furnitureMenu = new JMenu("Furniture");
+
+        furnitureMenu.add(new JMenuItem("Sofa"));
+        furnitureMenu.add(new JMenuItem("Table"));
+        furnitureMenu.add(new JMenuItem("Chair"));
+        furnitureMenu.add(new JMenuItem("Bed"));
+        furnitureMenu.add(new JMenuItem("Couch"));
+
+        return furnitureMenu;
+    }
+
+    private JMenu createClothingMenu() {
+        JMenu clothingMenu = new JMenu("Clothing");
+
+        clothingMenu.add(new JMenuItem("Women"));
+        clothingMenu.add(new JMenuItem("Men"));
+        clothingMenu.add(new JMenuItem("Kids"));
+        clothingMenu.add(new JMenuItem("Girls"));
+        clothingMenu.add(new JMenuItem("Boys"));
+
+        return clothingMenu;
+    }
+
+    private JMenu createGroceriesMenu() {
+        JMenu groceriesMenu = new JMenu("Grocery");
+
+        groceriesMenu.add(new JMenuItem("Fruits"));
+        groceriesMenu.add(new JMenuItem("Vegetables"));
+        groceriesMenu.add(new JMenuItem("Dairy Products"));
+        groceriesMenu.add(new JMenuItem("Rice"));
+        groceriesMenu.add(new JMenuItem("Snacks"));
+
+        return groceriesMenu;
+    }
 }
